72c8cdf870c79e06e5a4dd789a95b2ae8d0038f7
danielnguyen5070 <danielnguyen5070@gmail.com>
2026-09-13 22:14:49 +0700

    refactor(nav): remove shop and community links

--- config/navigation.ts
+++ config/navigation.ts
@@ -1,8 +1,6 @@
 export const NAV_LINKS = [
-  { href: "/#products", labelKey: "shop" },
   { href: "/blog", labelKey: "careGuide" },
   { href: "/#about", labelKey: "about" },
-  { href: "/#community", labelKey: "community" },
 ] as const;
 
 export const LOCALE_LABELS = {
